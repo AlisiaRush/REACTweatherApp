cefbb31e5dc7638a27d1c70e0bc60042e2023658
Alisia Rush <alisiarush@gmail.com>
2021-05-01 14:25:10 -0400

    Data shows based on specific coordinates 33.9462 and -84.3346 after user clicks on the submit button

--- weather/src/App.js
+++ weather/src/App.js
@@ -4,15 +4,15 @@ import axios from 'axios';
 
 const App = ()  => {
 
-  const [coordinatesXResults, setCoordinatesX] = useState([]);
-  const [coordinatesYResults, setCoordinatesY] = useState([]);
+  const [longitude, setCoordinatesX] = useState([]);
+  const [latitude, setCoordinatesY] = useState([]);
   const [cityResults, setCity] = useState([]);
   const [stateResults, setState] = useState([]);
   const [countyResults, setCounty] = useState([]);
   const [weatherResults, setWeather] = useState([]);
   
-
  const fetchData = () =>{
+
    const longLatCityStateApi = 'https://api.weather.gov/points/33.9462,-84.3346';
    const countyApi = 'https://api.weather.gov/zones/county/GAC089';
    const weatherApi = 'https://api.weather.gov/gridpoints/FFC/51,95/forecast';
@@ -24,19 +24,15 @@ const App = ()  => {
    axios.all([getlongLatCityState, getCounty, getWeather]).then(
      axios.spread((...allData) =>{
 
-      const xCoordinates = allData[0].data.geometry.coordinates[0];
-      const yCoordinates = allData[0].data.geometry.coordinates[1];
+      const longitude = allData[0].data.geometry.coordinates[0];
+      const latitude = allData[0].data.geometry.coordinates[1];
       const city = allData[0].data.properties.relativeLocation.properties.city;
       const state = allData[0].data.properties.relativeLocation.properties.state;
       const county = allData[1].data.properties.name;
       const weather = allData[2].data.properties.periods[0].temperature;
-      // console.log('AllLongLatCityState DATA', longLatCityStateAPI);
-      // console.log('ALLCounty DATA', countyAPI);
-      // console.log(getWeather);
-      // console.log('WEATHER', weather);
 
-      setCoordinatesX(xCoordinates);
-      setCoordinatesY(yCoordinates);
+      setCoordinatesX(latitude);
+      setCoordinatesY(longitude);
       setCity(city);
       setState(state);
       setCounty(county);
@@ -46,20 +42,53 @@ const App = ()  => {
  }
 
 useEffect(()=>{
-  fetchData()
+  
 }, []);
 
   return (
     <div className="App">
-        
-              Coordinates X: {coordinatesXResults}<br />
-              Coordinates Y: {coordinatesYResults}<br />
+
+              {/* Results */}
+                <div>
+              Results:<br />
+              Longitude: {longitude}<br />
+              Latutude : {latitude}<br />
               City: {cityResults}<br />
               State: {stateResults}< br />
 
               County: {countyResults}< br />
               Weather: {weatherResults}
+              </div>
+              
+            
+              {/* form */}
+<form>
+          
 
+              Longitude:
+              <input type="text"
+              value ={longitude}
+              onChange={(e) => setCoordinatesX(e.target.value)}>
+              </input>
+
+              Latitude:
+              <input type="text"
+              value ={latitude}
+              onChange={(e) => setCoordinatesY(e.target.value)}>
+              </input>
+              </form>
+
+              <p>Enter the following Coordinates:<br />
+                Longitude: 33.9462<br />
+                Latitude: -84.3346
+              </p>
+            <button
+            onClick = {()=>{
+              fetchData();
+            }}>
+              Submit
+            </button>
+            
     </div>
   );
 }
